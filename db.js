561c5260e710f8add586e6a647e95386155d5e3c
Rachit <rachitsen15@gmail.com>
2025-01-18 18:19:40 +0530

    new version for url

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 // const mongoURL = 'mongodb://localhost:27017/users';
-const mongoURL = process.env.DATAURL;
+const mongoURL =
+  "mongodb+srv://rachitsen015:becool15@cluster0.zwunm.mongodb.net/";
 
 //setup mongodb connection
 
